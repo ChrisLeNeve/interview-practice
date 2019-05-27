35f25af803aec2159572c07148dae59b6bf1cae9
Chris Neve <Christopher.NEVE.ext@ing.com>
2019-05-27 13:11:25 +0200

    More

--- initializers-poc/src/Hawk.java
+++ initializers-poc/src/Hawk.java
@@ -1,53 +1,84 @@
 
 class Bird {
+
+    private static int testStaticInt = setStaticInt();
+    private int testInt = setInt();
+    private static int STATIC_CONST = 4;
+    private int CONST = 3;
+
+    private int setInt() {
+        System.out.println("Set top parent non-static variables");
+        return 3;
+    }
+
+    private static int setStaticInt() {
+        System.out.println("Set top parent static variables");
+        return 2;
+    }
+
     static {
-        System.out.println("Top parent static initializer 1\n");
+        System.out.println("Top parent static initializer 1");
     }
     static {
-        System.out.println("Top parent static initializer 2\n");
+        System.out.println("Top parent static initializer 2");
     }
     {
-        System.out.println("Top parent initializer 1\n");
+        System.out.println("Top parent initializer 1");
     }
     {
-        System.out.println("Top parent initializer 2\n");
+        System.out.println("Top parent initializer 2");
     }
 
     public Bird() {
-        System.out.print("Top parent constructor\n");
+        System.out.println("Top parent constructor");
     }
 }
 
 class Raptor extends Bird {
+    private static int testStaticInt = setStaticInt();
+    private int testInt = setInt();
+
+    private int setInt() {
+        System.out.println("Set parent non-static variables");
+        return 4;
+    }
+
+    private static int setStaticInt() {
+        System.out.println("Set parent static variables");
+        return 4;
+    }
+
     static {
-        System.out.print("Parent static initializer 1\n");
+        System.out.println("Parent static initializer 1");
     }
     {
-        System.out.print("Parent initializer 1\n");
+        System.out.println("Parent initializer 1");
     }
     public Raptor() {
-        System.out.print("Parent constructor\n");
+        System.out.println("Parent constructor");
     }
     {
-        System.out.print("Parent initializer 2\n");
+        System.out.println("Parent initializer 2");
     }
     static {
-        System.out.print("Parent static initializer 2\n");
+        System.out.println("Parent static initializer 2");
     }
 }
 
-
 /**
- * Static initializers are called before anything else - before any code is executed.
- * After that, code starts executing.
- * When an object is instantiated, the initializers are called first, then the constructors.
- * In every case: called from the top down (from the top most parent down).
- * In both static initializers and "plain" initializers: if a class has several, they are called in that order.
+ * The order is as follows:
+ * 1. For every parent from the top-level down:
+ *  a. Static variables are set. At this point, non-static variables are not even available. (Variables assigned via a method and constants are treated exactly the same, in every case).
+ *  b. All its static initializers are called, in their declaration order.
+ * 2. Code starts executing.
+ * 3. For every parent from the top-level down:
+ *  a. Non-static variables are set.
+ *  b. All its non-static initializers are invoked, in their declaration order.
+ *  c. Constructors are invoked.
  */
 public class Hawk extends Raptor  {
     public static void main(String[] args) {
-        System.out.print("First line of code! in Main (class extends parent)\n");
+        System.out.println("\nFirst line of code! Now, we're going to initialize the child class.\n");
         new Hawk();
-        System.out.println("Last line in main\n");
     }
 }
